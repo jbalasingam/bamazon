c1aeec7cdc5111134c108398c94df20103f0c831
Jude Balasingam <mervinb@gmail.com>
2018-07-04 18:06:15 -0400

    initial site files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@
 var inquirer = require("inquirer");
 var db = require("./db");
 var currentProducts = "SELECT * FROM products";
+var selectById = "SELECT * FROM Bamazon.products WHERE id=";
 
 if(process.argv[2]=="insert"){
     command = "INSERT INTO products SET ?"
@@ -13,20 +14,11 @@ if(process.argv[2]=="insert"){
 var connection = db.login();
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-    // if (err) throw err;
+    if (err) throw err;
     // run the start function after the connection is made to prompt the user
     readProducts();
 });
   
-// console.log(command);
-// var inventoryObj = { 
-//     product: process.argv[3],
-//     department: process.argv[4],
-//     price: parseFloat(process.argv[5]),
-//     stock: parseFloat(process.argv[6])
-// };
-
-
 
 function insertProducts(){
     var query = connection.query(command, inventoryObj,
@@ -37,29 +29,82 @@ function insertProducts(){
 };
 
 function readProducts() {
-    console.log("See Below for the Current Products in Inventory.\n");
     connection.query(currentProducts, function(err, res, fields) {
       if (err) throw err;
-      // Log all results of the SELECT statement
-    //   var obj = JSON.stringify(res);
-    //   var obj = obj.split(",{");
-    //   console.log(obj);
+    console.log("See Below for the Current Products in Inventory.\n");
     Object.keys(res).forEach(function(key) {
         var row = res[key];
-        console.log("ID=" + row.id, row.product, "$"+row.price, row.stock+" units in stock.")
-    })
-
+        console.log("ID=" + row.id, row.product, "$"+row.price, row.stock+" units in stock.");
+    });
+    start();
 });
 };
+
+function checkUnits(myUnits) {
+    selectById = selectById + myID
+    connection.query(selectById, function(err, res, fields) {
+      if (err) throw err;
+    Object.keys(res).forEach(function(key) {
+        var row = res[key];
+        // console.log("ID=" + row.id, row.product, "$"+row.price,":", row.stock+" units in stock.");
+        availableUnits = row.stock;
+        price = row.price;
+    });
+
+    if(myUnits > availableUnits){
+        console.log("There are only " + availableUnits + " units available.")
+        recallUnits();
+    } else { 
+        Total = price * myUnits;
+        UnitsLeft = availableUnits - myUnits;
+        console.log("You owe $" + Total + ". THank you for shopping with Bamazon.")
+        readProducts();
+        console.log("There are " + UnitsLeft + " units left")
+        updateUnits(UnitsLeft,myID);
+        start();
+    }
+});
+};
+
+
 function start() {
-    inquirer
-      .prompt({
-        name: "product",
+    inquirer.prompt({
+        name: "number",
         type: "input",
         message: "Please enter the id of the product you seek."
       })
       .then(function(answer) {
         // based on their answer, ask how many units of the product they want.
-        console.log(answer);
+        myID = Number.parseInt(answer.number);
+        inquirer.prompt({
+            name: "number",
+            type: "input",
+            message: "How many units of the item would you like to buy?"
+          })
+          .then(function(answer) {
+            // based on their answer, ask how many units of the product they want.
+            myUnits = Number.parseInt(answer.number);
+            checkUnits(myUnits);
+          });
       });
+  };
+
+function recallUnits(){
+    inquirer.prompt({
+        name: "number",
+        type: "input",
+        message: "How many units of the item would you like to buy?"
+      })
+      .then(function(answer) {
+        // based on their answer, ask how many units of the product they want.
+        myUnits = Number.parseInt(answer.number);
+        console.log(myUnits);
+        checkUnits(myUnits);
+      });
+  };
+
+function updateUnits(UnitsLeft){
+    connection.query("UPDATE Bamazon.products SET stock=" + UnitsLeft + " WHERE id=" + myID +";", function(err, res, fields) {
+      if (err) throw err;
+    });
   };
