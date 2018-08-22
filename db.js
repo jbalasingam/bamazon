c9d38a4a0fc391306b84e296b588e4505bbec2ac
Jude Balasingam <mervinb@gmail.com>
2018-08-22 18:24:57 -0400

    added screenshots

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ function login() {
     var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
-        user: "root", 
-        password: "root", 
-        database: "bamazon", 
+        user: "root",
+        password: "root",
+        database: "Bamazon",
         multipleStatements: true
     });
 
